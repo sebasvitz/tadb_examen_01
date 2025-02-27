98deb8f1baead56b308ca02a075ff11b219bd19f
Natalia Urrego <natalia.urregor@upb.edu.co>
2025-02-26 21:50:34 -0500

    finalizacion postgres

--- Docker/Consultas.sql
+++ Docker/Consultas.sql
@@ -2,41 +2,73 @@
 ----- Natalia Urrego Rodriguez- 000365937
 
 ---------------------------CONSULTAS ------------------------------------------------
-
----CTM
-
-WITH CTE_Pagos_Empleado AS (
+---------------------------CTE----------------------------------------------------
+CREATE OR REPLACE VIEW resumen_cargos AS
+WITH CTE_Resumen AS (
     SELECT
-        e.id_cargo,
-        e.id AS empleado_id,
-        SUM(pq.monto) AS total_pagado_empleado
-    FROM pagosQuincenales pq
-    JOIN empleados e ON pq.id_empleado = e.id
-    GROUP BY e.id_cargo, e.id
+        c.id AS id_cargo,
+        c.nombre_cargo AS cargo,
+        COUNT(e.id) AS cantidad_personas,
+        SUM(c.remuneracion_quincena * e.cantidad_pi_trabajados * 2) AS total_pagado
+    FROM
+        cargos c
+    LEFT JOIN
+        empleados e ON c.id = e.id_cargo
+    GROUP BY
+        c.id, c.nombre_cargo
 )
 SELECT
-    c.id AS id_cargo,
-    c.nombre_cargo,
-    COUNT(CTE_Pagos_Empleado.empleado_id) AS total_empleados,
-    SUM(CTE_Pagos_Empleado.total_pagado_empleado) AS total_pagado
-FROM CTE_Pagos_Empleado
-JOIN cargos c ON CTE_Pagos_Empleado.id_cargo = c.id
-GROUP BY c.id, c.nombre_cargo
-ORDER BY c.id;
-
-
----WF-----------------
-SELECT 
-    id_sprint AS quincena,
-    SUM(monto) AS total_pagado,
-    COALESCE(LAG(SUM(monto)) OVER (ORDER BY id_sprint), 0) AS pago_anterior,
-    COALESCE(
-        ((SUM(monto) - LAG(SUM(monto)) OVER (ORDER BY id_sprint)) * 100 / 
-        NULLIF(LAG(SUM(monto)) OVER (ORDER BY id_sprint), 0)), 
-    0) AS variacion_porcentual
-FROM pagosQuincenales
-GROUP BY id_sprint
-ORDER BY id_sprint;
-
-
-
+    cargo,
+    cantidad_personas,
+    total_pagado
+FROM
+    CTE_Resumen;
+    
+    
+    -------------------------------------- WF-------------------------------------------------
+    ---------------------------------------------------------------------------------------
+    ----------------------------------------------------------------------------------------
+    
+CREATE OR REPLACE VIEW resumen_pagos_quincenales AS
+WITH CTE_PagosSprint AS (
+    SELECT
+        s.id AS id_sprint,
+        SUM(c.remuneracion_quincena) AS total_pagado_sprint
+    FROM
+        sprints s
+    JOIN
+        asignacionEquipo_PI aep ON s.id_PI = aep.id_pi
+    JOIN
+        equipos eq ON aep.id_equipo = eq.id
+    JOIN
+        empleados e ON eq.id = e.id_equipo
+    JOIN
+        cargos c ON e.id_cargo = c.id
+    GROUP BY
+        s.id
+),
+CTE_Variacion AS (
+    SELECT
+        id_sprint,
+        total_pagado_sprint,
+        LAG(total_pagado_sprint::INT, 1) OVER (ORDER BY id_sprint) AS total_pagado_sprint_anterior,
+        COALESCE(
+            ROUND(
+                ((total_pagado_sprint::INT - LAG(total_pagado_sprint::INT, 1) OVER (ORDER BY id_sprint)) / LAG(total_pagado_sprint::INT, 1) OVER (ORDER BY id_sprint)) * 100,
+                2
+            ),
+            0
+        ) AS variacion_porcentual
+    FROM
+        CTE_PagosSprint
+)
+SELECT
+    id_sprint AS sprint,
+    total_pagado_sprint AS total_pagado,
+    variacion_porcentual
+FROM
+    CTE_Variacion
+ORDER BY
+    id_sprint;
+ 
+ 
--- Docker/Esquema.sql
+++ Docker/Esquema.sql
@@ -55,11 +55,7 @@ GRANT ALL ON PROCEDURES TO examen01_usr;
 ---------------------------------Creacion de tablas--------------------------------------
 --**************************************************************************************
 
-------------------------------------------------------------------------------------------------
-------------------------------------------------------------------------------------------------
-			---------------   CREACION DE LAS TABLAS   -----------------
-------------------------------------------------------------------------------------------------
-------------------------------------------------------------------------------------------------
+
 -- Tabla Departamentos
 
 CREATE TABLE departamentos (
@@ -383,79 +379,7 @@ WHERE
 ORDER BY 
     e.id, s.id;
     
-    
-    
- -------------------------------------------------------------------------------------------------------
- ----------------------------- CONSULTAS -------------------------------------------------------------------------
- ------------------------------------------------------------------------------------------------------
- 
-CREATE OR REPLACE VIEW resumen_cargos AS
-WITH CTE_Resumen AS (
-    SELECT
-        c.id AS id_cargo,
-        c.nombre_cargo AS cargo,
-        COUNT(e.id) AS cantidad_personas,
-        SUM(c.remuneracion_quincena * e.cantidad_pi_trabajados * 2) AS total_pagado
-    FROM
-        cargos c
-    LEFT JOIN
-        empleados e ON c.id = e.id_cargo
-    GROUP BY
-        c.id, c.nombre_cargo
-)
-SELECT
-    cargo,
-    cantidad_personas,
-    total_pagado
-FROM
-    CTE_Resumen;
-    
-    
-    ---------------------------------------------------------------------------------------
-    
-CREATE OR REPLACE VIEW resumen_pagos_quincenales AS
-WITH CTE_PagosSprint AS (
-    SELECT
-        s.id AS id_sprint,
-        SUM(c.remuneracion_quincena) AS total_pagado_sprint
-    FROM
-        sprints s
-    JOIN
-        asignacionEquipo_PI aep ON s.id_PI = aep.id_pi
-    JOIN
-        equipos eq ON aep.id_equipo = eq.id
-    JOIN
-        empleados e ON eq.id = e.id_equipo
-    JOIN
-        cargos c ON e.id_cargo = c.id
-    GROUP BY
-        s.id
-),
-CTE_Variacion AS (
-    SELECT
-        id_sprint,
-        total_pagado_sprint,
-        LAG(total_pagado_sprint::INT, 1) OVER (ORDER BY id_sprint) AS total_pagado_sprint_anterior,
-        COALESCE(
-            ROUND(
-                ((total_pagado_sprint::INT - LAG(total_pagado_sprint::INT, 1) OVER (ORDER BY id_sprint)) / LAG(total_pagado_sprint::INT, 1) OVER (ORDER BY id_sprint)) * 100,
-                2
-            ),
-            0
-        ) AS variacion_porcentual
-    FROM
-        CTE_PagosSprint
-)
-SELECT
-    id_sprint AS sprint,
-    total_pagado_sprint AS total_pagado,
-    variacion_porcentual
-FROM
-    CTE_Variacion
-ORDER BY
-    id_sprint;
- 
- 
+
  
  
  -------------------------------------------------------------------------------------------------
